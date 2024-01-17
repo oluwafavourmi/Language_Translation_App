eedd5190971057ef75e309c5ce87bd1117a829a1
kenny akomolafe <akomolafekenny7@gmail.com>
2024-01-17 07:37:33 +0000

    language update

--- web.py
+++ web.py
@@ -3,14 +3,63 @@ from googletrans import Translator
 
 p = Translator()
 
-def translate(text):
+def french(text):
     translated = p.translate(text, dest='french')
+    return translated
+
+def german(text):
+    translated = p.translate(text, dest='german')
+    return translated
+
+def yoruba(text):
+    translated = p.translate(text, dest='yo')
+    return translated
+
+def Korean(text):
+    translated = p.translate(text, dest='korean')
+    return translated
+def chinese(text):
+    translated = p.translate(text, dest='zh-tw')
     return translated
 
 st.title('Language Translation App')
 text = st.text_input('Enter your text here')
 
-if st.button('Translate'):
-   output = translate(text)
-   st.success(output)
+option = st.sidebar.selectbox(
+                             'Select a Language:',
+                              ['French', 'German', 'Yoruba', 'Korean', 'Chinese'], index=0)
+st.sidebar.write('You selected:', option)
 
+mode = 1
+
+if option=='French':
+    mode = 1
+if option == 'German':
+    mode = 2
+if option == 'Yoruba':
+    mode = 3
+if option == 'Korean':
+    mode = 4
+if option == 'Chinese':
+    mode = 5
+
+if mode==1:
+     if st.button('Translate'):
+        output = french(text)
+        st.success(output)
+if mode==2:
+     if st.button('Translate'):
+        output = german(text)
+        st.success(output)
+if mode==3:
+     if st.button('Translate'):
+        output = yoruba(text)
+        st.success(output)
+if mode==4:
+     if st.button('Translate'):
+        output = Korean(text)
+        st.success(output)
+if mode==5:
+    if st.button('Translate'):
+        output = chinese(text)
+        st.success(output)
